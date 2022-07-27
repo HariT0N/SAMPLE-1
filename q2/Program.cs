edf40550da3f3fbdb9c91f81208f67f763a57c03
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-07-27 16:33:22 +0530

    q2 Completed

---
+++ q2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace q2
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+           
+            int x = GetInt(" Enter an Integer :");
+            PrintMessage(x.ToString());
+
+
+            int y = GetInt(" Enter an Integer :");
+            PrintMessage(y.ToString());
+
+            if (x == y)
+            { Console.WriteLine(" The given two intergers are equal"); }
+            else
+            { Console.WriteLine(" The given two intergers are not equal"); }
+        }
+         public static int GetInt(string message)
+    
+          {  
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+          }
+          public static void PrintMessage(string message)
+          {
+            Console.WriteLine(message);
+          }
+
+
+    }
+}
