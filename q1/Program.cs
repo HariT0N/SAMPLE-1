66775d8454579134aad9da51327c3d6067037611
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-07-27 16:19:09 +0530

    q1 Completed

---
+++ q1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int X = GetInt(" Enter an Integer :");
+            PrintMessage(X.ToString());
+            if (X > 0)
+            { 
+                Console.WriteLine(" Positive");
+                 }
+            else if (X == 0)
+            { 
+                Console.WriteLine(" Zero "); 
+                }
+            else
+            { 
+                Console.WriteLine(" Negative");
+                 }
+        }
+    
+           
+             public static int GetInt(string message)
+    
+          {  
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+          }
+          public static void PrintMessage(string message)
+          {
+            Console.WriteLine(message);
+
+
+    }
+    
+        
+}
+}
