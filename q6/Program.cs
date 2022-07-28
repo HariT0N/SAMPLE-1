b4354ff5e1e494eb0ed604f3f89151dc8ca01ad0
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-07-28 19:58:32 +0530

    q6 completed

---
+++ q6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace q6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age = GetInt(" Enter Age :");
+            PrintMessage(Age.ToString());
+
+            if (Age >= 18)
+            { Console.WriteLine(" The Candidate is eligible to vote"); }
+            else
+            { Console.WriteLine(" The Candidate is not eligible to vote"); }
+        }
+         
+             public static int GetInt(string message)
+    
+          {  
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+          }
+          public static void PrintMessage(string message)
+          {
+            Console.WriteLine(message);
+          }
+    }
+}
