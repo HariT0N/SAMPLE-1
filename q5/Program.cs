6e6aabc8cea2cd4d220c551de985e7190ad5d4d5
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-07-28 19:49:54 +0530

    q5 Completed

---
+++ q5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace q5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int Year = GetInt(" Enter a Year :");
+            PrintMessage(Year.ToString());
+
+            if (Year % 4 == 0 && Year % 100 != 0 || Year % 400 == 0)
+            { Console.WriteLine(Year + " is a Leap Year"); }
+            else
+            { Console.WriteLine(Year + " is not a Leap Year"); }
+        }
+         
+             public static int GetInt(string message)
+    
+          {  
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+          }
+          public static void PrintMessage(string message)
+          {
+            Console.WriteLine(message);
+          }
+        
+    }
+}
