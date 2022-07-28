c3bc6eb87a41eed3d6af7940843c8e8c092f74c7
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-07-28 16:58:41 +0530

    q4 Completed

---
+++ q4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace q4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int i = GetInt(" Enter an Integer :");
+            PrintMessage(i.ToString());
+
+            if (i % 2 == 0)
+            { Console.WriteLine(" The Number is Even"); }
+            else
+            { Console.WriteLine(" The Number is Odd"); }
+        }
+        public static int GetInt(string message)
+    
+          {  
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+          }
+          public static void PrintMessage(string message)
+          {
+            Console.WriteLine(message); 
+         }
+     }
+}
