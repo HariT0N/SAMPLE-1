a0f880215796aa1797d5a9e2cf97923f03738c3e
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-07-29 07:14:42 +0530

    q7 Completed

---
+++ q7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace q7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             float height = GetFloat(" enter the Height in centimetre:");
+            PrintMessage(height.ToString());
+
+            if (height <= 100)
+            { Console.WriteLine(" Short "); }
+            if (height > 100 && height < 150)
+            { Console.WriteLine(" Medium "); }
+            if (height >= 150)
+            { Console.WriteLine(" Tall "); }
+        }
+         public static float GetFloat(string message)
+         {
+            PrintMessage(message);
+            float temp = float.Parse(Console.ReadLine());
+            return temp;
+        }
+         public static void PrintMessage(string message)
+          {
+            Console.WriteLine(message);
+          }
+    }
+}
