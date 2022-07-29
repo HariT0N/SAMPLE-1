bd130e929b50fb897e812b243ed0b3e7ae31ede6
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-07-29 07:25:47 +0530

    q9 Completed

---
+++ q9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace q9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int d = GetInt(" Enter the first number :");
+            PrintMessage(d.ToString());
+            int e = GetInt(" Enter the second number :");
+            PrintMessage(e.ToString());
+            int f = GetInt(" Enter the third number :");
+            PrintMessage(f.ToString());
+
+            Console.WriteLine(" the smallest of three numbers is :" + smallest3(d, e, f));
+        }
+        public static int smallest3(int d, int e, int f)
+        {
+            int smallest = d;
+            if (e < smallest)
+            {smallest = e; }
+            if (f < smallest)
+            { smallest = f; }
+            return smallest;
+
+        }
+         public static int GetInt(string message)
+    
+          {  
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+          }
+          public static void PrintMessage(string message)
+          {
+            Console.WriteLine(message);
+          }
+    }
+}
