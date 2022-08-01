d0ca7317ee58897913ff7618cd445ff4c54fa1f5
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-08-01 16:00:44 +0530

    q1* completed

---
+++ q11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace q11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int angle1 = GetInt(" Enter the angle1 :");
+            PrintMessage(angle1.ToString());
+            int angle2 = GetInt(" Enter the angle2 :");
+            PrintMessage(angle2.ToString());
+            int angle3 = GetInt(" Enter the angle3 :");
+            PrintMessage(angle3.ToString());
+           System.Console.WriteLine(" the sum of three angles is : " + Triangle(angle1, angle2, angle3));
+            
+        }
+         public static double Triangle (int angle1, int angle2, int angle3)
+        {
+            int sum = angle1 + angle2 + angle3;
+            if (sum == 180)
+            { Console.WriteLine(" The three angles can form a Triangle "); }
+            else
+            { Console.WriteLine(" The Three angles cannot form a Triangle "); }
+            return sum;
+        }
+            
+             public static int GetInt(string message)
+        {
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+        }
+         public static void PrintMessage(string message)
+          {
+            Console.WriteLine(message);
+          }
+            
+        }
+}
+
---
+++ q12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace q12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = GetInt(" Enter a number :");
+            PrintMessage(N.ToString());
+
+            System.Console.WriteLine(N >= 0?(N==0?"zero":"Positive"):"Negative");
+
+
+        }
+         
+             public static int GetInt(string message)
+    
+          {  
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+          }
+          public static void PrintMessage(string message)
+          {
+            Console.WriteLine(message);
+          }
+    }
+}
