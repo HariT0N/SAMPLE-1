82a9c63dbac821c4ceb787a528698d29988cf201
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-08-02 07:57:53 +0530

    q5* Completed

---
+++ q14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace q14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Year = GetInt(" Enter an Year :");
+            PrintMessage(Year.ToString());
+            Console.WriteLine(IsLeapYear(Year));
+        }
+         public static bool IsTypicalLeapYear(int Year)
+        { return Year % 4 == 0; }
+        public static bool IsATypicalYear(int Year)
+        { return Year % 100 == 0 && Year % 400 != 0; }
+        public static bool IsLeapYear(int Year)
+        { return IsTypicalLeapYear(Year) && (IsATypicalYear(Year)); }
+
+          public static int GetInt(string message)
+        {
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+        }
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
