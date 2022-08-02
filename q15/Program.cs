eef8b666c30cdedae59ce1ab65ee904a8976ec3c
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-08-02 08:59:50 +0530

    q7*  Completed

---
+++ q15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace q15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int H = GetInt(" Enter the Height :");
+            PrintMessage(H.ToString());
+
+               Console.WriteLine(H <= 150 ?(H <= 100 ? " Short" :"Medium"): "Tall");
+
+          
+    }
+      public static int GetInt(string message)
+        {
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+        }
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+}
+}
