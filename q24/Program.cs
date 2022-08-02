5cb909ab04a86328ff7f3163a485d59ce6598bd1
HariT0N <19ust169.sjctni.edu@gmail.com>
2022-08-02 12:40:24 +0530

    q24 Completed

---
+++ q24/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace q23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            int SPrice = GetInt("Enter the Selling price: ");
+            PrintMessage(SPrice.ToString());
+            int CPrice = GetInt("Enter the Cost price: ");
+            PrintMessage(CPrice.ToString());
+
+            if ((SPrice - CPrice) > 0)
+            {
+                Console.WriteLine("It is a Profit");
+            }
+            else
+            {
+                Console.WriteLine("It is a Loss");
+            }
+
+          
+           }
+              public static int GetInt(string message)
+    
+          {  
+            PrintMessage(message);
+            int temp = int.Parse(Console.ReadLine());
+            return temp;
+          }
+          public static void PrintMessage(string message)
+          {
+            Console.WriteLine(message);
+          }
+
+
+
+}
+}
